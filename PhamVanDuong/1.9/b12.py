187a258fb1b9cd3e4ba60bff34e0cc8f3ba87b14
DuongPham05 <2005phama@gmail.com>
2026-04-07 07:19:05 +0700

    Add files via upload

---
+++ PhamVanDuong/1.9/b12.py
@@ -0,0 +1,9 @@
+lst = ['abc', 'xyz', 'aba', '1221', 'ii', 'ii2', '5yhy5']
+n = int(input("Nhập độ dài tối thiểu: "))
+
+count = 0
+for s in lst:
+    if len(s) >= n and s[0] == s[-1]:
+        count += 1
+
+print("Kết quả =", count)
